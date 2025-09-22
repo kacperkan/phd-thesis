8dc9e6f62a443277b5bc1095974d54feb101e9a9
Kacper Kania <kacp.kania@gmail.com>
2025-09-22 12:52:24 +0200

    Improve rebuttals

--- extract_rebuttals.sh
+++ extract_rebuttals.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mv aux/rebuttal_kk.pdf ./rebuttal_krzysztof_krawiec.pdf
-mv aux/rebuttal_bg.pdf ./rebuttal_boguslaw_cyganek.pdf
+cp aux/rebuttal_kk.pdf ./rebuttal_krzysztof_krawiec.pdf
+cp aux/rebuttal_bg.pdf ./rebuttal_boguslaw_cyganek.pdf
