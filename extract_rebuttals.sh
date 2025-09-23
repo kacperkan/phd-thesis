7012593ed9550e34e1145ae43bb080e60f92395c
Kacper Kania <kacp.kania@gmail.com>
2025-09-23 21:39:48 +0200

    Move files differentely

--- extract_rebuttals.sh
+++ extract_rebuttals.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cp aux/rebuttal_kk.pdf ./rebuttal_krzysztof_krawiec.pdf
-cp aux/rebuttal_bg.pdf ./rebuttal_boguslaw_cyganek.pdf
+if [[ ! -d "krzysztof_krawiec" ]] 
+then
+    mkdir krzysztof_krawiec
+fi
+
+if [[ ! -d "boguslaw_cyganek" ]] 
+then
+    mkdir boguslaw_cyganek
+fi
+ 
+cp aux/rebuttal_kk.pdf krzysztof_krawiec/rebuttal_kacper_kania.pdf
+cp aux/rebuttal_bg.pdf boguslaw_cyganek/rebuttal_kacper_kania.pdf
